819d111eea9faecc521eed3883708ce4a74dd9b5
ibTanjeeb <threepointonefour1993@gmail.com>
2022-02-15 20:15:31 +0600

    added new

--- BookListRazor/BookListRazor/Cotroller/BookController.cs
+++ BookListRazor/BookListRazor/Cotroller/BookController.cs
@@ -1,5 +1,6 @@
 ﻿using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,24 @@ namespace BookListRazor.Cotroller
             _db = db;
         }
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task <IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() });
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult>Delete(int id)
+        {
+            var BookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+
+            if(BookFromDb==null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+             _db.Book.Remove (BookFromDb);
+            await _db.SaveChangesAsync();
+
+            return Json(new { success = true, message = "Delete Successfully" });
         }
     }
 }
